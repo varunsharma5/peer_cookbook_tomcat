14ea1ba755ad6ec1b1f6590fb0408f425f97adb6
Varun Sharma <varun.sharma@progress.com>
2021-08-01 20:09:21 +0530

    fixed all linting error

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'tomcat'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Varun Sharma'
+maintainer_email 'varunge@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures tomcat'
 version '0.1.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,6 @@
 # Cookbook:: tomcat
 # Recipe:: default
 #
-# Copyright:: 2021, The Authors, All Rights Reserved. 
+# Copyright:: 2021, The Authors, All Rights Reserved.
 include_recipe 'tomcat::install_java'
 include_recipe 'tomcat::install_tomcat'
