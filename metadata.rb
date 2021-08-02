c70deb301f75ddad16678947b6d88547cf7184a6
Varun Sharma <varun.sharma@progress.com>
2021-08-02 10:53:28 +0530

    added comment to check cookbook version check logic

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures tomcat'
 version '0.1.0'
 chef_version '>= 15.0'
-
+# Testing cookbook version check
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
