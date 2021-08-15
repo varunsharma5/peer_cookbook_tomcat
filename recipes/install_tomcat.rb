4d980bb2412af6341c5950c0d992265b2dfd37e7
Varun Sharma <varun.sharma@progress.com>
2021-08-15 16:14:08 +0530

    Added not_if gaurds in execute resource

--- recipes/install_tomcat.rb
+++ recipes/install_tomcat.rb
@@ -19,6 +19,7 @@ end
 # tomcat_package = Chef::Config[:file_cache_path] + '/tomcat.tar.gz'
 # tomcat_tar_ball = "tomcat-#{node[tomcat][version]}"
 tomcat_package = "/tmp/tomcat-#{node['tomcat']['version']}.tar.gz"
+tomcat_home_dir = '/opt/tomcat'
 # Download the Tomcat Binary
 remote_file tomcat_package do
   source node['tomcat']['download_url']
@@ -26,20 +27,23 @@ remote_file tomcat_package do
 end
 
 # Extract the Tomcat Binary
-# TODO: version check and make it idempotent
 execute 'extract_tomcat' do
-  command "sudo tar xvf #{tomcat_package} -C /opt/tomcat --strip-components=1"
+  command "sudo tar xvf #{tomcat_package} -C #{tomcat_home_dir} --strip-components=1"
   cwd '/tmp'
+  not_if "java -cp #{tomcat_home_dir}/lib/catalina.jar org.apache.catalina.util.ServerInfo | /usr/bin/grep #{node['tomcat']['version']}"
+  not_if { ::File.directory?("#{tomcat_home_dir}/lib") }
 end
 
 # Update the Permissions
 # TODO: change to group and chmod resource
 execute 'update_permission' do
-  cwd '/opt/tomcat'
-  command 'sudo chgrp -R tomcat /opt/tomcat'
+  cwd tomcat_home_dir
+  command "sudo chgrp -R tomcat #{tomcat_home_dir}"
   command 'sudo chmod -R g+r conf'
   command 'sudo chmod g+x conf'
   command 'sudo chown -R tomcat webapps/ work/ temp/ logs/ bin/ lib/ conf/'
+  not_if "java -cp #{tomcat_home_dir}/lib/catalina.jar org.apache.catalina.util.ServerInfo | /usr/bin/grep #{node['tomcat']['version']}"
+  not_if { ::File.directory?("#{tomcat_home_dir}/lib") }
 end
 
 # Install the Systemd Unit File
