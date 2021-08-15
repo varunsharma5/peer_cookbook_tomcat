18128cd9c463c11a28cd7d8a1659f03d4dc34a91
Varun Sharma <varun.sharma@progress.com>
2021-08-15 18:27:44 +0530

    modified for adding gaurds when tomcat version is changed. Made it fully idempotent

--- recipes/install_tomcat.rb
+++ recipes/install_tomcat.rb
@@ -16,10 +16,9 @@ user 'tomcat' do
   action :create
 end
 
-# tomcat_package = Chef::Config[:file_cache_path] + '/tomcat.tar.gz'
-# tomcat_tar_ball = "tomcat-#{node[tomcat][version]}"
 tomcat_package = "/tmp/tomcat-#{node['tomcat']['version']}.tar.gz"
 tomcat_home_dir = '/opt/tomcat'
+
 # Download the Tomcat Binary
 remote_file tomcat_package do
   source node['tomcat']['download_url']
@@ -30,33 +29,33 @@ end
 execute 'extract_tomcat' do
   command "sudo tar xvf #{tomcat_package} -C #{tomcat_home_dir} --strip-components=1"
   cwd '/tmp'
+  action :run
   not_if "java -cp #{tomcat_home_dir}/lib/catalina.jar org.apache.catalina.util.ServerInfo | /usr/bin/grep #{node['tomcat']['version']}"
-  not_if { ::File.directory?("#{tomcat_home_dir}/lib") }
 end
 
 # Update the Permissions
-# TODO: change to group and chmod resource
 execute 'update_permission' do
   cwd tomcat_home_dir
   command "sudo chgrp -R tomcat #{tomcat_home_dir}"
   command 'sudo chmod -R g+r conf'
   command 'sudo chmod g+x conf'
-  command 'sudo chown -R tomcat webapps/ work/ temp/ logs/ bin/ lib/ conf/'
-  not_if "java -cp #{tomcat_home_dir}/lib/catalina.jar org.apache.catalina.util.ServerInfo | /usr/bin/grep #{node['tomcat']['version']}"
-  not_if { ::File.directory?("#{tomcat_home_dir}/lib") }
+  command 'sudo chown -R tomcat LICENSE webapps/ work/ temp/ logs/ bin/ lib/ conf/'
+  not_if { Etc.getpwuid(::File.stat("#{tomcat_home_dir}/LICENSE").uid).name == 'tomcat' }
 end
 
 # Install the Systemd Unit File
-template '/etc/systemd/system/tomcat.service' do
+template 'Systemd Unit File' do
+  path '/etc/systemd/system/tomcat.service'
   source 'tomcat.service.erb'
+  notifies :reload, 'systemd_unit[daemon-reload]', :immediately
 end
 
 # Reload Systemd to load the Tomcat Unit file
 systemd_unit 'daemon-reload' do
-  action :reload
+  action :nothing
 end
 
 # Ensure tomcat is started and enabled
 service 'tomcat' do
-  action [:enable, :start]
+  action [:enable, :restart]
 end
