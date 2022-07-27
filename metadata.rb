b9505b80ca9a19cce41ced8a15f48d50a09b6934
Varun Sharma <varunge@gmail.com>
2022-07-27 10:49:55 +1000

    Test Trigger tomcat 8_5

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Varun Sharma'
 maintainer_email 'varunge@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures tomcat'
-version '1.1.16'
+version '1.1.17'
 chef_version '>= 15.0'
 # Testing cookbook version check...
 # The `issues_url` points to the location where issues for this cookbook are
